bbd7752ac683d02974b0e1c2daa70e0b4a21f59a
collinlor <61028772+collinlor@users.noreply.github.com>
2021-03-01 19:58:13 -0600

    Update Run_SPAdes.py

--- Run_SPAdes.py
+++ Run_SPAdes.py
@@ -19,20 +19,24 @@ def main():
     #CHANGE ME: change this to represent the alphabetic components to the genomic 'tags'
     label = 'UMB'
     
+    #CHANGE ME: change this to either 'Y' to only output the contigs.fasta file or 'N' to output all files
+    toPrune = 'Y'
+    
+    #making the new directory
     try:
         os.mkdir(new)
     except PermissionError:
         os.system('sudo mkdir ' + new)
-    except FileExistsError:
+    except FileExistsError: #if the directory already exists
         pass
 
-    runSPAdes(spades, original, new)
+    runSPAdes(spades, original, new, label, which, toPrune)
 
-def runSPAdes(spades, original, new):
+def runSPAdes(spades, original, new, label, which, pruner):
     #cycle through all quality trimmed reads
     for read in os.listdir(original):
         r1name = str(read)
-
+            
         #check which read out of R1 or R2
         r1test = r1name.find('R1')
         if r1test != -1:
@@ -40,24 +44,33 @@ def runSPAdes(spades, original, new):
         else:
             pass
 
-        #need to record the UMB#### tag
-        tagLoc = r1name.find('UMB')
-        if r1name[tagLoc+6].isdigit(): 
-            #UMB four digits
-            tag = r1name[tagLoc:tagLoc+7]
-        else:
-            #UMB three digits
-            tag = r1name[tagLoc:tagLoc+6]
-
-        #create new directory using UMB#### as name
+        #need to record the tag for current file
+        #must pass the r1name and label to findTag() function
+        tag = findTag(r1name,label)
+        
+        #end program if the tag cannot be found
+        if not type(tag) is string:
+            return
+        
+        #create new directory using file tags as name
         try:
             os.mkdir(new + tag)
         except PermissionError:
             os.system('sudo mkdir ' + new + tag)
+        except FileExistsError: #the directory already exists
+            print('Warning: ' + new + tag + ' is an existing directory. \n Output will be placed in directory, but output files may be under modified names.')
+            pass
+        
         outputDir = new+tag
 
         #form spades command
-        command = 'python3 ' + spades + ' -1 ' + original + r1name + ' -2 ' + original + r2name + ' -o ' + outputDir
+        #Must adhere to the specified data type
+        if which == 'D': #DNA
+            command = 'python3 ' + spades + ' -1 ' + original + r1name + ' -2 ' + original + r2name + ' -o ' + outputDir
+        elif which == 'R': #RNA
+            command = 'python3 ' + spades + ' --rna -1 ' + original + r1name + ' -2 ' + original + r2name + ' -o ' + outputDir
+        elif which == 'M' #Metagenomic
+            command = 'python3 ' + spades + ' --meta -1 ' + original + r1name + ' -2 ' + original + r2name + ' -o ' + outputDir
             
         #test
         #print(tag)
@@ -70,10 +83,34 @@ def runSPAdes(spades, original, new):
         except PermissionError:
             os.system('sudo ' + command)
 
-        #get rid of all SPAdes output files except contigs.fasta
-        prune(new, outputDir, tag)
+        #Check if user wants to prune output files
+        if pruner == 'Y':
+            #get rid of all SPAdes output files except contigs.fasta
+            prune(new, outputDir, tag)
         #break
+
+#For correct filing of paired reads by recognizing
+#numbering after the user-given label
+def findTag(fileName, label):
+    tagLoc = fileName.find(label)
+
+    #make sure user has input correct label
+    if tagLoc == -1:
+        print('ERROR: filing label cannot be detected. The given label:\n' + label + '\n cannot be found within filename: ' + fileName+ '\n Please check the user input.')
+        return
     
+    labelLen = len(label)
+    
+    #loop through file name until the first non-number after label
+    for spot in range(tagLoc+labelLen, len(fileName)):
+        status = fileName[spot].isdigit()
+        if status == True: #the char is still a number
+            continue
+        else:
+            return fileName[tagLoc:spot]
+
+#Deletes all output files except for contigs.fasta
+#also adds the sequence tag to contigs.fasta
 def prune(new,sir, tag):
     #function to delete every SPAdes output except contigs.fasta
     for files in os.listdir(sir):
